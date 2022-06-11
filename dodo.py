ed3d46240f365f275cbc3807b7bda77d76165426
rt2122 <alisa.nemeshaeva@gmail.com>
2022-06-11 17:38:39 +0300

    Basic configs for build & automation.

---
+++ dodo.py
@@ -0,0 +1,54 @@
+"""Doit file."""
+
+DOIT_CONFIG = {'default_tasks': ['all']}
+
+
+def task_html():
+    """Make HTML documentation."""
+    return {
+            'actions': ['sphinx-build -b html docs/source docs/build'],
+           }
+
+
+def task_sdist():
+    """Create source distribution."""
+    return {
+            'actions': ['python -m build -s'],
+           }
+
+
+def task_wheel():
+    """Create binary wheel distribution."""
+    return {
+            'actions': ['python -m build -w'],
+           }
+
+
+def task_style():
+    """Check style with flake8."""
+    return {
+            'actions': ['flake8 ADL']
+           }
+
+
+def task_docstyle():
+    """Check docstrings with pydocstyle."""
+    return {
+            'actions': ['pydocstyle ADL']
+           }
+
+
+def task_check():
+    """Perform all checks."""
+    return {
+            'actions': None,
+            'task_dep': ['style', 'docstyle']
+           }
+
+
+def task_all():
+    """Perform all build task."""
+    return {
+            'actions': None,
+            'task_dep': ['check', 'html', 'wheel']
+           }
