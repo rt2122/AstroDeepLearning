d90a8ad4bb55d5a9f8cd73519398298190f0ccdf
rt2122 <alisa.nemeshaeva@gmail.com>
2022-08-05 18:05:49 +0300

    Calculate test metrics added in script.

--- ADL/model/scripts.py
+++ ADL/model/scripts.py
@@ -8,7 +8,7 @@ import os
 def train_Planck_Unet(model_name: str, data_path: str, target_path: str, model_path: str,
                       pixels: str, pretrained: str, batch_size: str, epochs: str,
                       device: str, continue_train: bool = False, lr_scheduler: str = None,
-                      save_best_only: bool = False) -> None:
+                      save_best_only: bool = False, test_as_val: bool = False) -> None:
     """Full process of training.
 
     :param data_path: Path to data.
@@ -51,6 +51,13 @@ def train_Planck_Unet(model_name: str, data_path: str, target_path: str, model_p
                                  pix2=pix_dict["val"], batch_size=int(batch_size))
     dataset_val.prepare()
 
+    dataset_test = None
+    if test_as_val:
+        dataset_test = Planck_Dataset(data_path=data_path, target_path=target_path,
+                                      pix2=pix_dict["test"], batch_size=int(batch_size))
+        dataset_test.prepare()
+
     model = ADL_Unet(os.path.join(model_path, model_name + "_ep{epoch:03}.hdf5"), weights=weights,
-                     lr_scheduler=lr_scheduler, save_best_only=save_best_only)
+                     lr_scheduler=lr_scheduler, save_best_only=save_best_only,
+                     test_as_val=dataset_test)
     model.train(dataset_train, dataset_val, int(epochs), continue_train=continue_train)
