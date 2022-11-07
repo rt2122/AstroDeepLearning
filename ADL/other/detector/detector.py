62e597c770dc2fd5a74363b6968ce2799626f7ff
rt2122 <alisa.nemeshaeva@gmail.com>
2022-11-07 23:21:16 +0300

    Fix fast scan for n classes.

--- ADL/other/detector/detector.py
+++ ADL/other/detector/detector.py
@@ -116,7 +116,7 @@ def fast_skan_sky_Planck(data_path: str, out_path: str, model_path: str, nside: 
     :rtype: None
     """
     model = Unet_model(weights=model_path)
-    fast_model = Unet_model(n_classes=1, **model_prms)
+    fast_model = Unet_model(**model_prms)
     fast_model.set_weights(model.get_weights())
     X = [np.load(os.path.join(data_path, f'{ipix}.npy')) for ipix in range(hp.nside2npix(2))]
     if lfi_path:
