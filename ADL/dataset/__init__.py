afd19a4421bb035aeae89c9f2b57c77e802a6aff
rt2122 <alisa.nemeshaeva@gmail.com>
2023-03-07 15:03:28 +0300

    Added init files.

--- ADL/dataset/__init__.py
+++ ADL/dataset/__init__.py
@@ -1,3 +1,4 @@
 """Init file."""
 from .Planck import Planck_Dataset
-__all__ = ["Planck_Dataset"]
+from .Planck_torch import Planck_Regression_Dataset
+__all__ = ["Planck_Dataset", "Planck_Regression_Dataset"]
--- ADL/model/__init__.py
+++ ADL/model/__init__.py
@@ -1,4 +1,5 @@
 """Module with Deep Learnging models."""
 from .Unet_tf import ADL_Unet, Unet_model
+from .MDN_Regression_torch import MLP_GMM, loss_f
 
-__all__ = ["ADL_Unet", "Unet_model"]
+__all__ = ["ADL_Unet", "Unet_model", "MLP_GMM", "loss_f"]
