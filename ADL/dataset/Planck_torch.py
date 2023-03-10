fa071efb8c3698833244952717ee3aef81357843
rt2122 <alisa.nemeshaeva@gmail.com>
2023-03-10 15:28:37 +0300

    Added augmentation.

--- ADL/dataset/Planck_torch.py
+++ ADL/dataset/Planck_torch.py
@@ -5,13 +5,33 @@ import os
 from typing import List
 from matplotlib import pyplot as plt
 import matplotlib
+from torchvision.transforms import Compose, RandomHorizontalFlip, RandomVerticalFlip
+import random
+import torchvision.transforms.functional as TF
+
+
+class MyRotationTransform:
+    """Rotate by one of the given angles."""
+
+    def __init__(self, angles: List[float]):
+        self.angles = angles
+
+    def __call__(self, x):
+        angle = random.choice(self.angles)
+        return TF.rotate(x, angle)
 
 
 class Planck_Regression_Dataset(torch.utils.data.Dataset):
     """Planck_Regression_Dataset."""
 
-    def __init__(self, data_path: str, target_path: str, pix2: List[int], patch_size: int = 32,
-                 reg_prm: str = "M500"):
+    def __init__(
+        self,
+        data_path: str,
+        target_path: str,
+        pix2: List[int],
+        patch_size: int = 32,
+        reg_prm: str = "M500",
+    ):
         """Initialize dataset.
 
         :param data_path: Planck data path (npy files).
@@ -31,17 +51,28 @@ class Planck_Regression_Dataset(torch.utils.data.Dataset):
         self.pix2 = pix2
         self.patch_size = patch_size
         self._prepare()
+        self.transforms = Compose(
+            [
+                RandomHorizontalFlip(0.5),
+                RandomVerticalFlip(0.5),
+                MyRotationTransform([90 * i for i in range(4)]),
+            ]
+        )
 
     def _prepare(self):
         """_prepare."""
         self.data = {}
         for i in self.pix2:
-            self.data[i] = np.load(os.path.join(self.data_path, f"{i}.npy")).astype(np.float32)
+            self.data[i] = np.load(os.path.join(self.data_path, f"{i}.npy")).astype(
+                np.float32
+            )
 
         target = pd.read_csv(self.target_path)
 
         if not set(["x", "y", "pix2", self.reg_prm]).issubset(list(target)):
-            raise(ValueError("Dataset table should have x, y, pix2 & reg_prm columns."))
+            raise (
+                ValueError("Dataset table should have x, y, pix2 & reg_prm columns.")
+            )
 
         # Remove objects that are too close to grid and won't fit
         target = target[np.in1d(target["pix2"], self.pix2)]
@@ -65,9 +96,10 @@ class Planck_Regression_Dataset(torch.utils.data.Dataset):
         size = self.patch_size // 2
         image = self.data[line["pix2"]]
 
-        image = image[x - size: x + size, y - size: y + size]
+        image = image[x - size : x + size, y - size : y + size]
         image = torch.tensor(image)
         image = torch.permute(image, [2, 0, 1])
+        image = self.transforms(image)
 
         return image, line[self.reg_prm]
 
@@ -84,8 +116,8 @@ class Planck_Regression_Dataset(torch.utils.data.Dataset):
         X, y = self[idx]
         rows, cols = 2, 3
         f, ax = plt.subplots(rows, cols, figsize=(12, 8))
-        for i in range(X.shape[-1]):
-            ax[i // cols][i % cols].imshow(X[:, :, i])
+        for i in range(X.shape[0]):
+            ax[i // cols][i % cols].imshow(X[i, :, :])
         ax[0][0].set_title("{} = {:.2f}".format(self.reg_prm, y))
 
         f.tight_layout()
