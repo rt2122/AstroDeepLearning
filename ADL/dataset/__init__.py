d3d0d75321e37d2f232f0801057bcf9f5b99ddce
rt2122 <alisa.nemeshaeva@gmail.com>
2023-06-27 20:17:53 +0300

    Add docs for dataset

--- ADL/dataset/__init__.py
+++ ADL/dataset/__init__.py
@@ -1,1 +1,6 @@
 """Init file."""
+from .dataset import do_aug
+from .Planck import Planck_Dataset
+from .Planck_torch import Planck_Regression_Dataset, StratifiedSampler
+
+__all__ = ["do_aug", "Planck_Dataset", "Planck_Regression_Dataset", "StratifiedSampler"]
